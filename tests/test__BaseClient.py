540694cd806a9ad3c138a29b6257b85d3c60678e
john.downs <john.downs@yodle.com>
2015-02-10 16:36:01 -0500

    cleanup assertions

--- tests/test__BaseClient.py
+++ tests/test__BaseClient.py
@@ -8,18 +8,21 @@ client = _BaseClient.BaseClient(host)
 def test_search_uses_correct_parameters(get):
     client.search(q="abc=123,def=ghi")
 
-    get.assert_called_once_with(host + '/v1/search?q=abc=123,def=ghi', data={}, headers={'content-type': 'application/json'})
+    get.assert_called_once_with(host + '/v1/search?q=abc=123,def=ghi', data='""',
+                                headers={'content-type': 'application/json'}, auth=None)
 
 @patch('_BaseClient.get')
 def test_check_status_calls_correct_url(get):
     client.check_status()
-    get.assert_called_once_with(host + '/v1/_ping', data={}, headers={'content-type': 'application/json'})
+    get.assert_called_once_with(host + '/v1/_ping', data='""',
+                                headers={'content-type': 'application/json'}, auth=None)
 
 @patch('_BaseClient.get')
 def test_get_image_layer_calls_correct_url(get):
     image_id = '12345'
     client.get_image_layer(image_id)
-    get.assert_called_once_with(host + '/v1/images/12345/layer', data={}, headers={'content-type': 'application/json'})
+    get.assert_called_once_with(host + '/v1/images/12345/layer', data='""',
+                                headers={'content-type': 'application/json'}, auth=None)
 
 @patch('_BaseClient.put')
 def test_put_image_layer_calls_correct_url_with_body(put):
@@ -27,10 +30,12 @@ def test_put_image_layer_calls_correct_url_with_body(put):
     image_id = '12345'
     image_data = '0x0123456789ABCDEF'  # Just some binary data
     client.put_image_layer(image_id, image_data)
-    put.assert_called_once_with(host + '/v1/images/12345/layer', data=image_data, headers={'content-type': 'application/json'})
+    put.assert_called_once_with(host + '/v1/images/12345/layer', data='"' + image_data + '"',
+                                headers={'content-type': 'application/json'}, auth=None)
 
 @patch('_BaseClient.get')
 def test_put_image_metadata_calls_correct_url(get):
     image_id = '12345'
     client.get_image_metadata(image_id)
-    get.assert_called_once_with(host + '/v1/images/12345/json', data={}, headers={'content-type': 'application/json'})
+    get.assert_called_once_with(host + '/v1/images/12345/json', data='""',
+                                headers={'content-type': 'application/json'}, auth=None)
